51109df729cd721d68332dfd8b19e9aa840f1dce
saviktor-yandex <94538734+saviktor-yandex@users.noreply.github.com>
2021-12-06 16:12:38 +0500

    Продолжение

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Again')
-
+    print('UFO came and added this line')
+    my_superfunction()
 
 
 if __name__ == '__main__':
