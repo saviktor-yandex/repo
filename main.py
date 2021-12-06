3be6e554bdbf4fc20030eb22b8aa6fc8e5e625b2
saviktor-yandex <94538734+saviktor-yandex@users.noreply.github.com>
2021-12-06 16:12:12 +0500

    Начало

---
+++ main.py
@@ -0,0 +1,13 @@
+def my_superfunction():
+    print('What an awesome print!')
+
+
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+
+
+
+if __name__ == '__main__':
+    main()
